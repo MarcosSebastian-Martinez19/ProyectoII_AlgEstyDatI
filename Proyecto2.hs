e47ac15148fb5816cf64da61357b7ea4525f1256
Marcos Martinez <marcossebastian429@gmail.com>
2023-09-11 13:02:50 -0300

    Updates

--- Proyecto2.hs
+++ Proyecto2.hs
@@ -63,10 +63,10 @@ type Altura = Int
 type NumCamiseta = Int
 
 -- Tipos algebráicos sin parámetros (aka enumerados)
-data Zona = Arco | Defensa | Mediocampo | Delantera
-data TipoReves = DosManos | UnaMano
-data Modalidad = Carretera | Pista | Monte | BMX
-data PiernaHabil = Izquierda | Derecha
+data Zona = Arco | Defensa | Mediocampo | Delantera deriving Show
+data TipoReves = DosManos | UnaMano deriving Show
+data Modalidad = Carretera | Pista | Monte | BMX deriving Show
+data PiernaHabil = Izquierda | Derecha deriving Show
 
 -- Sinónimo
 type ManoHabil = PiernaHabil
@@ -77,7 +77,7 @@ data Deportista = Ajedrecista                           -- Constructor sin argum
     | Velocista Altura                                  -- Constructor con un argumento
     | Tenista TipoReves ManoHabil Altura                -- Constructor con tres argumentos
     | Futbolista Zona NumCamiseta PiernaHabil Altura    -- Constructor con 4 argumentos
-
+    deriving Show
 --b ¿Cuál es el tipo del constructor Ciclista?
 -- El tipo del constructor Ciclista es toma una Modalidad y devuelve un tipo Deportista
 
@@ -133,11 +133,11 @@ verificarZona _ _ = False
 --0
 
 -- La segunda
---contar_futbolistas' :: [Deportista] -> Zona -> Int
---contar_futbolistas' [] z = 0
---contar_futbolistas' (Futbolista zona _ _ _ :xs) z = case verificarZona z zona of
---    True -> 1 + contar_futbolistas xs z
---    False -> contar_futbolistas xs z
+contar_futbolistas' :: [Deportista] -> Zona -> Int
+contar_futbolistas' [] z = 0
+contar_futbolistas' (Futbolista zona _ _ _ :xs) z = case verificarZona z zona of
+    True -> 1 + contar_futbolistas xs z
+    False -> contar_futbolistas xs z
 -- Prueba
 --ghci> contar_futbolistas' [Futbolista Arco 10 Izquierda 180, Futbolista Arco 5 Derecha 170, Ciclista Pista, Ajedrecista, Futbolista Defensa 12 Izquierda 190] Arco
 --2
@@ -251,115 +251,35 @@ sonidoCromatico (Nota nota alteracion) =
 
 -- e
 -- Incluí el tipo NotaMusical a la clase Eq de manera tal que dos notas que tengan el mismo valor de sonidoCromatico se consideren iguales.
--- Definamos Eq para Alteracion
--- Defini Eq para alteracion también porque sino me decía que no podia comprara NotaMusical porque Alteración no tenia la Clase Eq
-
-instance Eq Alteracion
-    where
-        Bemol == Bemol = True
-        Natural == Natural = True
-        Sostenido == Sostenido = True
-        _ == _ = False
-
--- Prueba
---ghci> Bemol == Bemol
---True
---ghci> Natural == Natural
---True
---ghci> Sostenido == Sostenido
---True
---ghci> Bemol == Natural
---False
---ghci> Bemol == Sostenido
---False
---ghci> Natural == Sostenido
---False
 
 -- Definamos Eq para NotaMusical
 
 instance Eq NotaMusical
     where
-        (Nota nota1 alteracion1) == (Nota nota2 alteracion2) = nota1 == nota2 && alteracion1 == alteracion2
+        (Nota nota1 alteracion1) == (Nota nota2 alteracion2) = sonidoCromatico(Nota nota1 alteracion1) == sonidoCromatico(Nota nota2 alteracion2)
 
 -- Prueba
---ghci> let notaMusical1 = Nota Do Bemol
---ghci> let resultado1 = sonidoCromatico notaMusical1
---ghci> let notaMusical2 = Nota Re Natural
---ghci> let resultado2 = sonidoCromatico notaMusical2
---ghci> let notaMusical3 = Nota Mi Sostenido
---ghci> let resultado3 = sonidoCromatico notaMusical3
---ghci> let notaMusical4 = Nota Fa Bemol
---ghci> let resultado4 = sonidoCromatico notaMusical4
---ghci> let notaMusical5 = Nota Sol Natural
---ghci> let resultado5 = sonidoCromatico notaMusical5
---ghci> let notaMusical6 = Nota La Sostenido
---ghci> let resultado6 = sonidoCromatico notaMusical6
---ghci> let notaMusical7 = Nota Si Natural
---ghci> let resultado7 = sonidoCromatico notaMusical7
---ghci> resultado1 == resultado2
---False
---ghci> resultado1 == resultado3
---False
---ghci> resultado2 == resultado4
---False
---ghci> resultado3 == resultado5
---False
---ghci> resultado5 == resultado6
---False
---ghci> resultado1 == resultado1
---True
---ghci> resultado2 == resultado2
---True
---ghci> resultado3 == resultado3
---True
---ghci> resultado4 == resultado4
---True
---ghci> resultado5 == resultado5
---True
---ghci> resultado6 == resultado6
---True
---ghci> resultado7 == resultado7
---True
+-- ghci> Nota Sol Natural  == Nota Sol Sostenido
+-- False
+-- ghci> Nota Mi Sostenido == Nota Mi Sostenido
+-- True
+-- ghci> Nota Do Bemol == Nota Do Bemol
+-- True
 
 --f
 -- Definamos Ord para NotaMusical
 -- Preguntar en clase si está bien
 
 instance Ord NotaMusical where
-    (Nota nota1 alteracion1) <= (Nota nota2 alteracion2) =
-        case compare (sonidoCromatico (Nota nota1 alteracion1)) (sonidoCromatico(Nota nota2 alteracion2)) of
-            LT -> True -- LT devuelve True porque LT considera (sonidoCromatico (Nota nota1 alteracion1)) es menor que (sonidoCromatico(Nota nota2 alteracion2))
-            EQ -> True -- EQ devuelve True porque EQ considera (sonidoCromatico (Nota nota1 alteracion1)) es igual que (sonidoCromatico(Nota nota2 alteracion2))
-            GT -> False -- GT devuelve False porque GT considera (sonidoCromatico (Nota nota1 alteracion1)) es mayor que (sonidoCromatico(Nota nota2 alteracion2))
--- compare :: Ord a => a -> a -> Ordering
+    (Nota nota1 alteracion1) <= (Nota nota2 alteracion2) = sonidoCromatico (Nota nota1 alteracion1) <= sonidoCromatico(Nota nota2 alteracion2)
 
 -- Prueba
---ghci> let notaMusical1 = Nota Do Bemol
---ghci> let resultado1 = sonidoCromatico notaMusical1
---ghci> let notaMusical2 = Nota Re Natural
---ghci> let resultado2 = sonidoCromatico notaMusical2
---ghci> let notaMusical3 = Nota Mi Sostenido
---ghci> let resultado3 = sonidoCromatico notaMusical3
---ghci> let notaMusical4 = Nota Fa Bemol
---ghci> let resultado4 = sonidoCromatico notaMusical4
---ghci> let notaMusical5 = Nota Sol Natural
---ghci> let resultado5 = sonidoCromatico notaMusical5
---ghci> let notaMusical6 = Nota La Sostenido
---ghci> let resultado6 = sonidoCromatico notaMusical6
---ghci> let notaMusical7 = Nota Si Natural
---ghci> let resultado7 = sonidoCromatico notaMusical7
---ghci> resultado1 <= resultado2
---True
---ghci> resultado1 <= resultado3
---True
---ghci> resultado1 <= resultado1
---True
---ghci> resultado5 <= resultado1
---False
---ghci> resultado5 <= resultado2
---False
---ghci> resultado7 <= resultado6
---False
+-- ghci> Nota Do Bemol <= Nota Do Bemol
+-- True
+-- ghci> Nota Mi Sostenido <= Nota Mi Sostenido
+-- True
+-- ghci> Nota Mi Sostenido <= Nota Re Natural
+-- False
 
 -- Ejercicio 6 Tipos enumerados con polimorfismo.
 
@@ -401,37 +321,6 @@ primerElemento'' (x:xs) = Just (x)
 --ghci> primerElemento'' ["x","a"]
 --Just "x"
 
--- Definimos Show para Deportista Sino no me meuestra nada la funcion atender
-
-instance Show Zona where
-    show Arco = "Arco"
-    show Defensa = "Defensa"
-    show Mediocampo = "Mediocampo"
-    show Delantera = "Delantera"
-
-instance Show TipoReves where
-    show DosManos = "DosManos"
-    show UnaMano = "UnaMano"
-
-instance Show Modalidad where
-    show Carretera = "Carretera"
-    show Pista = "Pista"
-    show Monte = "Monte"
-    show BMX = "BMX"
-
-instance Show PiernaHabil where
-    show Izquierda = "Izquierda"
-    show Derecha = "Derecha"
-
-instance Show Deportista where
-    show Ajedrecista = "Ajedrecista"
-    show (Ciclista modalidad) = "Ciclista " ++ show modalidad
-    show (Velocista altura) = "Velocista " ++ show altura
-    show (Tenista tipoReves manoHabil altura) =
-        "Tenista " ++ show tipoReves ++ " " ++ show manoHabil ++ " " ++ show altura
-    show (Futbolista zona numCamiseta piernaHabil altura) =
-        "Futbolista " ++ show zona ++ " " ++ show numCamiseta ++ " " ++ show piernaHabil ++ " " ++ show altura
---
 
 -- Ejercicio 7 Tipos Recursivos
 -- Definición del tipo Cola
@@ -442,27 +331,18 @@ instance Show Cola where
     show VaciaC = "VaciaC"
     show (Encolada deportista cola) = show deportista ++ ", " ++ show cola
 
--- Inciso a
+-- Inciso adata Cola = VaciaC | Encolada Deportista Cola
 -- 1
 atender :: Cola -> Maybe Cola
 atender VaciaC = Nothing
 atender (Encolada _ cola) = Just cola
 
 -- Prueba
-
---ghci> let cola1 = Encolada Ajedrecista VaciaC
---ghci> let resultado1 = atender cola1
---ghci> print resultado1
---Just VaciaC
-
---ghci> let cola2 = Encolada (Futbolista Arco 1 Derecha 175) (Encolada Ajedrecista VaciaC)
---ghci> let resultado2 = atender cola2
---ghci> print resultado2
---Just Ajedrecista, VaciaC
-
---ghci> let cola3 = Encolada (Ciclista BMX) (Encolada Ajedrecista (Encolada (Ciclista Monte) VaciaC))
---ghci> let resultado3 = atender cola3
---ghci> print resultado3
+-- ghci> atender (Encolada Ajedrecista (Encolada (Futbolista Arco 1 Derecha 175) VaciaC))
+-- Just Futbolista Arco 1 Derecha 175, VaciaC
+-- ghci> atender (Encolada Ajedrecista VaciaC)
+-- Just VaciaC
+--ghci> atender (Encolada (Ciclista BMX) (Encolada Ajedrecista (Encolada (Ciclista Monte) VaciaC)))
 --Just Ajedrecista, Ciclista Monte, VaciaC
 
 -- 2
@@ -476,3 +356,37 @@ encolar d (Encolada deportista cola) = Encolada deportista (encolar d cola)
 --Ciclista BMX, VaciaC
 --ghci> encolar (Ciclista BMX) (Encolada Ajedrecista (Encolada (Futbolista Arco 1 Derecha 190) VaciaC))
 --Ajedrecista, Futbolista Arco 1 Derecha 190, Ciclista BMX, VaciaC
+
+-- 3
+busca :: Cola -> Zona -> Maybe Deportista
+busca VaciaC _ = Nothing
+busca (Encolada deportista cola) z = case deportista of
+    Futbolista z _ _ _  -> Just deportista
+    _ -> busca cola z
+
+
+
+
+
+
+
+
+
+
+
+
+-- Ejercicio 8 Tipos recursivos y polimórficos.
+data ListaAsoc a b = Vacia | Nodo a b (ListaAsoc a b)
+
+type Diccionario = ListaAsoc String String
+type Padron = ListaAsoc Int String
+
+-- A
+type GuiaTelefonica = ListaAsoc Nombre Int
+type Nombre = String
+
+-- B
+-- 1
+la_long :: ListaAsoc a b -> Int
+la_long Vacia = 0
+la_long (Nodo x y la) = 1 + (la_long la)
